d7d95db3570f0fdff04947709e36312b70c75a60
thaliaamiraa <thalia02amira@gmail.com>
2023-07-12 00:30:10 +0700

    dsb

--- application/views/admin/view/dashboard.php
+++ application/views/admin/view/dashboard.php
@@ -6,7 +6,7 @@
         <img src="<?= base_url() ?>assets/images/simpokda.png" width="100" heigth="100" >
         </div>
         <h3>SELAMAT DATANG "ADMINISTRATOR"</h3>
-        <h5>Di Sistem Pengelompokan Desa Berdasarkan Dampak Bencana Menggunakan Metode K-Means Clustering 2023</h5>
+        <h5>Di Sistem Pengelompokan Desa Berdasarkan Dampak Bencana Menggunakan Metode K-Means Clustering 2023 by Thalia Amira Rifda</h5>
       </div>
     </div>
   </div>
